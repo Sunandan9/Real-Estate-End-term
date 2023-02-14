d664865c4abdcdfc4e925cc106259aa68ff730f1
Sunandan9 <sunandan0709.be20@chitkara.edu.in>
2023-02-14 13:58:04 +0530

    Add all my files

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../StyleSheet/Footer.css";
+const Footer = () => {
+  return (
+    <div className="Footer__container">
+      <div className="Footer__companyBrand">
+        <h3>Sunandan Sharma</h3>
+      </div>
+      <div className="Footer__licensing">
+        <h4>©2023 | All Rights Reseverd</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
